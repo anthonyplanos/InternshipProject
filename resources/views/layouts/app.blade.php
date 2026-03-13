293dfc37277bc2ce39e077c28ad11a59f1ba64a3
Anthony Planos <planosant14@gmail.com>
2026-03-13 14:29:41 +0800

    Livewire package

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        @livewireStyles
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -32,5 +34,8 @@
                 {{ $slot }}
             </main>
         </div>
+
+        @livewireStyles
+        @livewireScripts
     </body>
 </html>
